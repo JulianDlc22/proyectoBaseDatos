08689e815a1481c9143a66f4e223e99ca83de4ad
JulianDlc <ja.dlcastillo@gmail.com>
2025-08-28 23:14:42 -0600

    ingreso producto y categorias, guardar y importar los objetos en un archivo.txt

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ class Producto:
         self.id_producto = id_producto
         self.nombre = nombre
         self.id_categoria = id_categoria
-        self.precio = precio
-        self.total_compra = total_compra
-        self.total_ventas = total_ventas
-        self.stock = stock
+        self.precio = float(precio)
+        self.total_compra = int(total_compra)
+        self.total_ventas = int(total_ventas)
+        self.stock = int(stock)
 
 class Categoria:
     def __init__(self,id_categoria,nombre):
@@ -51,7 +51,7 @@ class Ventas:
 
 class DetalleVentas:
     def __init__(self, id_detalle_venta, id_venta, cantidad, id_producto, precio, subtotal):
-        self.id_venta = id_detalle_venta
+        self.id_detalle_venta = id_detalle_venta
         self.id_venta = id_venta
         self.cantidad = cantidad
         self.id_producto = id_producto
@@ -82,48 +82,48 @@ class Gestiones:
     def __init__(self):
         pass
 
-class RepositorioGeneral:
-
-    def cargar(self):
-        pass
-    def guardar(self):
-        pass
-
-class RepositorioProductos(RepositorioGeneral):
-
-    def cargar(self):
-     pass
-
-
-class GestionProductos:
-    def __init__(self):
-        self.productos = {}
-        self.cargar_clientes()
+class RepositorioProductosGenerico:
 
     def cargar_productos(self):
+        pass
+    def guardar_productos(self):
+        pass
+
+class RepositorioProductosTxt(RepositorioProductosGenerico):
+    def __init__(self):
+        self.productos = {}
+        self.cargar_productos()
+
+    def cargar_productos(self):
+
         try:
             with open("productos.txt", "r", encoding="utf-8") as archivo:
                 for linea in archivo:
                     linea = linea.strip()
                     if linea:
                         id_producto, nombre, id_categoria, precio, total_compra, total_ventas, stock = linea.split(":")
-                        self.productos[id_producto] = {
-                            "nombre": nombre,
-                            "id_categoria": id_categoria,
-                            "precio": precio,
-                            "total_compra": total_compra,
-                            "total_ventas": total_ventas,
-                            "stock": stock
+                        self.productos[id_producto] = Producto(id_producto, nombre, id_categoria, precio, total_compra,total_ventas, stock)
 
-                        }
-            print("Productos importados desde productos.txt")
+                print("Productos importados desde productos.txt")
+
         except FileNotFoundError:
             print("No existe el archivo productos.txt, se creará uno nuevo al guardar.")
+
 
     def guardar_productos(self):
         with open("productos.txt", "w", encoding="utf-8") as archivo:
             for id_producto, datos in self.productos.items():
-                archivo.write(f"{id_producto}:{datos['nombre']}:{datos['id_categoria']}:{datos['precio']}:{datos['total_compra']}:{datos['total_ventas']}:{datos['stock']}\n")
+                archivo.write(f"{id_producto}:{datos.nombre}:{datos.id_categoria}:{datos.precio}:{datos.total_compra}:{datos.total_ventas}:{datos.stock}\n")
+
+
+
+class GestionProductos:
+    def __init__(self, repositorio_productos: RepositorioProductosTxt , repositorio_categorias : "RepositorioCategorias"):
+        self.repositorio_productos = repositorio_productos
+        self.repositorio_categorias = repositorio_categorias
+        self.productos = self.repositorio_productos.productos
+        self.categorias = self.repositorio_categorias.categorias
+
 
     def agregar_producto(self):
         print("\n--Ingreso de Productos--")
@@ -135,7 +135,7 @@ class GestionProductos:
             while True:
                 id_producto = input("Codigo del Producto: ")
                 if id_producto in self.productos:
-                    input("Codigo ingresado ya en exitencia, presione para volver a Ingresarlo")
+                    input("Codigo ingresado ya en existencia, presione para volver a Ingresarlo")
                     continue
                 break
 
@@ -143,7 +143,7 @@ class GestionProductos:
 
             while True:
                 id_categoria = input("ID de la categoria del Producto: ")
-                if len(self.categoria) == 0:
+                if len(self.categorias) == 0:
                     input("Aun no hay categorias ingresadas, presione para volver a Ingresarlo")
                     continue
                 else:
@@ -163,7 +163,7 @@ class GestionProductos:
             while True:
                 try:
                     stock = int(input("Stock del Producto: "))
-                    if precio <= 0:
+                    if stock <= 0:
                         input("error- stock no valido, presione para volver a Ingresarlo ")
                         continue
 
@@ -171,12 +171,22 @@ class GestionProductos:
                 except ValueError:
                     input("error- Stock no valido, presione para volver a Ingresarlo")
 
-            self.productos[id_producto] = Producto(id_producto, nombre, id_categoria, precio, 0 , 0, stock)
-            print("\nProducto Agregado Exitosamente...")
+            producto = Producto(id_producto, nombre, id_categoria, precio, 0 , 0, stock)
+            self.productos[id_producto] = producto
 
             print(" ")
-            self.guardar_productos()
+            self.repositorio_productos.guardar_productos()
             print(f"Producto Guardado con codigo {id_producto} agregado y guardado correctamente.")
+            confirmacion = input("Desea Ingresar otro producto? presione S para continuar o presione N para salir: ").lower()
+
+            if confirmacion == "s":
+                continue
+            elif confirmacion == "n":
+                break
+            else:
+                print("error- opcion no valida, se terminara el ingreso")
+                break
+
 
     def mostrar_todos(self):
         if self.productos:
@@ -188,12 +198,79 @@ class GestionProductos:
         else:
             print("No hay clientes registrados.")
 
+class RepositorioCategorias():
+    def __init__(self):
+        self.categorias = {}
+        self.cargar_categorias()
+
+    def cargar_categorias(self):
+
+        try:
+            with open("categorias.txt", "r", encoding="utf-8") as archivo:
+                for linea in archivo:
+                    linea = linea.strip()
+                    if linea:
+                        id_categoria, nombre = linea.split(":")
+                        self.categorias[id_categoria] = Categoria(id_categoria, nombre)
+
+                print("Categorias importados desde productos.txt")
+
+        except FileNotFoundError:
+            print("No existe el archivo productos.txt, se creará uno nuevo al guardar.")
+
+    def guardar_categorias(self):
+        with open("categorias.txt", "w", encoding="utf-8") as archivo:
+            for id_categoria, datos in self.categorias.items():
+                archivo.write(
+                    f"{id_categoria}:{datos.nombre}\n")
+
+class GestionCategorias:
+
+    def __init__(self, repositorio: RepositorioCategorias):
+        self.repositorio = repositorio
+        self.categorias = self.repositorio.categorias
+
+    def agregar_categorias(self):
+        print("\n--Ingreso Categorias--")
+        i = 0
+        while True:
+
+            print(f"\nCategoria No.{i + 1}")
+
+            while True:
+                id_categoria = input("Codigo de la categoria: ")
+                if id_categoria in self.categorias:
+                    input("Codigo ingresado ya en existencia, presione para volver a Ingresarlo")
+                    continue
+                break
+
+            nombre = input("Nombre del Producto: ").lower()
+
+            categoria = Categoria(id_categoria, nombre)
+            self.categorias[id_categoria] = categoria
+
+            print(" ")
+            self.repositorio.guardar_categorias()
+            print(f"Producto Guardado con codigo {id_categoria} agregado y guardado correctamente.")
+            confirmacion = input(
+                "Desea Ingresar otro producto? presione S para continuar o presione N para salir: ").lower()
+
+            if confirmacion == "s":
+                continue
+            elif confirmacion == "n":
+                break
+            else:
+                print("error- opcion no valida, se terminara el ingreso")
+                break
+
+
+
 #clase menu#
 
 class Menu:
-    def __init__(self):
+    def __init__(self, gestion_productos: GestionProductos, gestion_categorias: GestionCategorias):
         self.opcionesMenu= {
-            "1": MenuAdmin(),
+            "1": MenuAdmin(gestion_productos = gestion_productos,gestion_categorias= gestion_categorias ),
             "2": MenuVendedor()
         }
 
@@ -202,13 +279,38 @@ class OpcionesMenu:
         pass
 
 class MenuAdmin(OpcionesMenu):
+    def __init__(self, gestion_productos : GestionProductos, gestion_categorias : GestionCategorias):
+        self.gestion_productos = gestion_productos
+        self.gestion_categorias = gestion_categorias
+
     def ejecutar(self):
-        print("--Menu Admin--")
-        print("1. Ingresar Producto")
-        print("2. Ingresar Empleado")
-        print("3. Ingresar Proveedor")
-        print("4. Ingresar Compra")
-        print("5: Salir")
+
+        while True:
+            print("--Menu Admin--")
+            print("1. Ingresar Producto")
+            print("2. Ingresar Empleado")
+            print("3. Ingresar Proveedor")
+            print("4. Ingresar Compra")
+            print("5: Salir")
+
+            opcion = input("eliga una opcion: ")
+            try:
+                op = int(opcion)
+
+                match op:
+                    case 1:
+                        self.gestion_productos.agregar_producto()
+                    case 2:
+                        self.gestion_categorias.agregar_categorias()
+                    case 5:
+                        break
+                    case _:
+                        input("opcion fuera de rango permitido, presione enter para continuar...")
+
+            except ValueError:
+                input("opcion invalida, presione para continuar...")
+
+
 
 class MenuVendedor(OpcionesMenu):
     def ejecutar(self):
@@ -230,7 +332,6 @@ class IngresoUsuario:
                 print("Usuario Valido, Bienvenido Admin")
                 input("Presione enter para continuar...")
                 return 1
-                break
 
             else:
                 cont -= 1
@@ -247,7 +348,13 @@ class IngresoUsuario:
 
 #programa Principal
 admin = IngresoUsuario()
-menu = Menu()
+repositorio_productos = RepositorioProductosTxt()
+repositorio_categorias = RepositorioCategorias()
+gestiones_productos = GestionProductos(repositorio_productos,repositorio_categorias)
+gestiones_categorias = GestionCategorias(repositorio_categorias)
+menu = Menu(gestiones_productos, gestiones_categorias)
+
+
 while True:
     print("1. Administrador")
     print("2. Vendedor")
@@ -260,26 +367,20 @@ while True:
         match opcion:
             case 1:
                 admin.validacion_admin()
-
-                if IngresoUsuario == 1:
-                    while True:
-                        menu.opcionesMenu["1"].ejecutar()
-
+                menu.opcionesMenu["1"].ejecutar()
             case 2:
                 pass
             case 3:
-                pass
+                print("Saliendo...")
+                break
             case _:
                 print("ERROR- opcion fuera del rango permitido")
-                input(print("Presione enter para continuar..."))
-
-
-
+                input("Presione enter para continuar...")
 
 
     except ValueError:
         print("Error- esta Ingresando una opcion invalida")
-        input(print("Presione enter para continuar..."))
+        input("Presione enter para continuar...")
 
 
 
